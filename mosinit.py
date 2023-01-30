531bdfaf24993ff5a9a6cb49adc7ccda6f69ad9a
Alexandru Săvulescu <alexandru.savulescu@epfl.ch>
2023-01-30 19:47:51 +0100

    Migrate to Python3 (#1)

--- mosinit.py
+++ mosinit.py
@@ -1,2 +1,2 @@
-execfile("fig2a_run.py")
-execfile("fig2a_show.py")
+exec(compile(open("fig2a_run.py", "rb").read(), "fig2a_run.py", 'exec'))
+exec(compile(open("fig2a_show.py", "rb").read(), "fig2a_show.py", 'exec'))
